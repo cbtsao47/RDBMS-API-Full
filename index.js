9626e9e85805c263256f7e7d75f7c8023506f736
wisdomtooth <cbtsao47@gmail.com>
2019-01-16 18:33:54 -0600

    clear console logs

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`you are not insane`);
+  console.log(`Sanity Check`);
 });
